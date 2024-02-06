b41e938c2dc85e782035ad1e2a9c10d48eab20a2
Fernanda Morfin <159086645+FernyMorfin@users.noreply.github.com>
2024-02-06 15:17:57 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('hello wrold')
+# Author: Fernanda Morfin
+# My first Python lab in my CIS129 course
+# Prints a simple sad short story :'(
+print('Monkey has no taxes. Monkey has stick')
+print('Monkey is happy')
+print('Man has no stick')
+print('Man has taxes')
+print('Man sad')
+print('the end')
